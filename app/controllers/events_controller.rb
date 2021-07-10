168f1f2406b5d880aeb3401d7ef963c74e848214
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-10 12:54:48 +0900

    eventsコントローラーにnew, createメソッドを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.includes(:admin_user).order('created_at DESC')
   end
+
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  end
 end
