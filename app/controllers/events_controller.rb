3fe55fadf75a11e330e151509753f842dc6d3fd5
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-10 18:35:52 +0900

    eventsコントローラーのcreateアクションを実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,5 +9,15 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:event_image, :name, :event_date, :content).merge(admin_user_id: current_user.id)
   end
 end
