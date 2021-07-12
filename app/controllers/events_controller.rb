80b51f391cf9d5eed4068d2bb7c456d285182740
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-12 22:09:28 +0900

    イベント削除機能を実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_action :authenticate_admin_user!, except: [:index, :show]
+  before_action :set_event, only: [:show, :destroy]
+  before_action :admin_user_authentication, only:[:destroy]
   def index
     @events = Event.includes(:admin_user).order('created_at DESC')
   end
@@ -20,10 +23,25 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def destroy
+    @event.destroy
+    redirect_to root_path
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:event_image, :name, :event_date,
                                   :content).merge(admin_user_id: current_admin_user.id)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def admin_user_authentication
+    unless current_admin_user.id == @event.admin_user_id
+      redirect_to root_path
+    end
   end
 end
