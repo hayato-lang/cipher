873c10011e161500f5a5eb36b430e1c280e31093
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-15 20:57:10 +0900

    profile欄の修正

--- app/models/admin_profile.rb
+++ app/models/admin_profile.rb
@@ -2,6 +2,5 @@ class AdminProfile < ApplicationRecord
   belongs_to :admin_user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
