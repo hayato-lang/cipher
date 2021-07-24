1be5185de5a2af5b42b08f2675fc22a7c9a2e8b7
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-24 18:39:10 +0900

    factorybotの実装

--- spec/factories/admin_profiles.rb
+++ spec/factories/admin_profiles.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :admin_profile do
-    
+    postal_code { Faker::Address.zip }
+    prefecture_id {'24'}
+    municipality {Faker::Address.city}
+    address {Faker::Address.street_name}
+    building_name {Faker::Address.street_address}
+    phone_number {Faker::PhoneNumber.cell_phone}
+    profile {Faker::Base.regexify}
+    association :admin_user
+    after(:build) do |admin|
+      admin.admin_image.attach(io: File.open('public/images/test_admin_image.jpg'), filename: 'test_admin_image.jpg')
+    end
   end
 end
+
