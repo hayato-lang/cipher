38a81fea14c860cf271c676ee9fe8d73a17c9e07
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-07-26 20:59:58 +0900

    show.htmlにadmin_userのevent一覧を表示

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,6 +1,8 @@
 class AdminUsersController < ApplicationController
   def show
     admin_user = AdminUser.find(params[:id])
+    @store_name = admin_user.store_name
     @events = admin_user.events
+    @admin_profile = admin_user.admin_profile
   end
 end
