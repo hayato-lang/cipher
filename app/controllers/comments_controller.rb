62bf70892c1c4f12f1b04a5e241d3409f9a12a8b
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-08-01 12:27:10 +0900

    commentを投稿

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(text: params[:comment][:text])
+    @comment = Comment.new(comment_params)
     if @comment.save
-      ActionCable.server.broadcast.'comment_channel', content: @comment
+      ActionCable.server.broadcast 'comment_channel', content: @comment
     end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(event_id: params[:event_id])
   end
 end
