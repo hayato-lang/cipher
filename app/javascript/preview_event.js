1d17837759f44bc2effd961573ccfc3cd141ff3f
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-08-09 21:37:24 +0900

    editページもプレビュー機能を追加

--- app/javascript/preview_event.js
+++ app/javascript/preview_event.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('event-image')){
-    const ImageChoice = document.getElementById('image-choice')
-    document.getElementById('event-image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+    const ImageChoice = document.getElementById('image-choice');
+
+    const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
       const blobImage = document.createElement('img');
+      imageElement.setAttribute('class','pre-img');
       blobImage.setAttribute('src', blob);
       imageElement.appendChild(blobImage);
-      ImageChoice.appendChild(imageElement)
+      ImageChoice.appendChild(imageElement);
+    };
+
+    document.getElementById('event-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
     });
   }
 });
