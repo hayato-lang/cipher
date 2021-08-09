e1e7faee90d78282647c070d18361b912499ee3b
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-08-09 20:47:29 +0900

    event/newのpreviewページを実装

--- app/javascript/preview_event.js
+++ app/javascript/preview_event.js
@@ -1,2 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
+  if ( document.getElementById('event-image')){
+    const ImageChoice = document.getElementById('image-choice')
+    document.getElementById('event-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageChoice.appendChild(imageElement)
+    });
+  }
 });
