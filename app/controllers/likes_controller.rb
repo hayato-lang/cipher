a431d38c72568dbd9f68e26b6a01c5ce6a4bec18
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-09-25 08:51:41 +0900

    ロボコップによる記述修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,17 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!, only: %i[destroy create]
   before_action :post_params
-  
+
   def create
     Like.create(user_id: current_user.id, event_id: params[:id])
   end
-  
+
   def destroy
     Like.find_by(user_id: current_user.id, event_id: params[:id]).destroy
   end
-  
+
   private
-  
+
   def post_params
     @event = Event.find(params[:id])
   end
